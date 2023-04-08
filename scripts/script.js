d474aed2fbc8efe6b77f27ea9b88e06853882b18
David Laptop <davidnguyenantran@gmail.com>
2023-04-07 21:57:09 -0700

    Adds function to toggle cancel save and textarea visibility and adds function to save a note into an array with the title showing in nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const theme = document.querySelector("#theme");
 const navB = document.querySelector("nav");
+const txtArea = document.querySelector("textarea");
+const saveCancel = document.querySelector("#savecancel");
+const mainGrid = document.querySelector(".maingrid");
+const noteList = document.querySelector("nav ul");
 
 function themeToggle(event) {
   console.log(event);
@@ -9,5 +13,45 @@ function themeToggle(event) {
     ? (theme.textContent = "Light Theme")
     : (theme.textContent = "Dark Theme");
 }
-
 theme.addEventListener("click", themeToggle);
+
+function cancel(event) {
+  if (event.target.textContent === "Cancel") {
+    txtArea.classList.add("hide");
+    saveCancel.classList.add("hide");
+  }
+}
+saveCancel.addEventListener("click", cancel);
+
+function newNote(event) {
+  console.log(event);
+  if (event.target.textContent === "New Note") {
+    txtArea.classList.remove("hide");
+    saveCancel.classList.remove("hide");
+    txtArea.value = "";
+  }
+}
+mainGrid.addEventListener("click", newNote);
+
+let notesArray = [
+  { title: "note one", body: "this is my first note" },
+  { title: "note two", body: "this is my second note" },
+];
+
+function saveNote(event) {
+  console.log(event);
+  if (event.target.textContent === "Save") {
+    let noteText = txtArea.value;
+    let noteTitle = prompt("What is the title of this note?");
+    let fullNote = { title: noteTitle, body: noteText };
+    notesArray.push(fullNote);
+
+    let note = document.createElement("li");
+    noteList.appendChild(note);
+    note.textContent = noteTitle;
+    console.log(note);
+    console.log(notesArray);
+  }
+}
+
+saveCancel.addEventListener("click", saveNote);
